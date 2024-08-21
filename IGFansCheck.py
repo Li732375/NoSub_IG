5e343a4cae2fa9088ac26c4c5014adbeff09165a
Li732375 <lee732375@gmail.com>
2024-08-21 16:23:30 +0800

    Update IGFansCheck.py
    新增顯示資訊供參考，但清單存取功能不全障礙尚存

--- IGFansCheck.py
+++ IGFansCheck.py
@@ -6,8 +6,15 @@ L = instaloader.Instaloader()
 
 L.login(cre[0], cre[1])
 
+# 載入 Session 使用
+#L.load_session_from_file('')
+    
+
 selfname = instaloader.Profile.own_profile(L.context).username
 profile = instaloader.Profile.from_username(L.context, selfname)
+print(f"追蹤數： {profile.followees}")
+print(f"粉絲數： {profile.followers}")
+print(f"合計差異數為 {profile.followees - profile.followers}")
 
 # followings > followers
 followings = []
@@ -16,15 +23,16 @@ followers = []
 for i in profile.get_followees():
     followings.append(i.username)
 
-# 這裡直接空撈，還不報錯
-print(f"followers:\n{profile.get_followers()}")
-for i in profile.get_followers():
+for i in profile.get_followers(): # 這裡直接空撈，還不報錯
     followers.append(i.username)
+
+# 儲存 Session 供下次使用
+L.save_session_to_file()
 
 followings_set = set(followings)
 followers_set = set(followers)
-print(f"追蹤數： {len(followings_set)}")
-print(f"粉絲數： {len(followers_set)}")
+print(f"取得的追蹤清單數量： {len(followings_set)}")
+print(f"取得的追蹤者清單數量： {len(followers_set)}")
 
 # 求出兩名單之間的差異帳號
 delta_set = followings_set - followers_set
